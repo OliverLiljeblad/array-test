00f0a4397f8e369ff09400080b6acec950b58a20
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2021-10-25 20:37:27 +0200

    Made a arrey test

---
+++ ArrayTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wishlist = new string[3];
+
+            Console.WriteLine("Skriv in 3 saker du önskar dig");
+
+            for (int i = 0; i < wishlist.Length; i++)
+            {
+                wishlist[i] = Console.ReadLine();
+            }
+
+            Array.Sort(wishlist);
+            //    for (int i = 0; i < wishlist.Length; i++)
+            //   {
+            //     Console.WriteLine(wishlist[i]);
+            //   }
+
+            foreach (var item in wishlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
